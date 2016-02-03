38a08da1cb3da51afc7ef402dc7a54383ae90a6a
Tiago Santos <theflisnton@gmail.com>
2016-02-03 14:18:53 +0000

    Show a cell in the table view for each venue Search for venues when the keyboard search button is pressed

--- FQSearch/FQSearchTableViewController.m
+++ FQSearch/FQSearchTableViewController.m
@@ -7,92 +7,76 @@
 //
 
 #import "FQSearchTableViewController.h"
+#import "FQVenue.h"
+#import "FQSearchService.h"
 
-@interface FQSearchTableViewController ()
+@interface FQSearchTableViewController () <UISearchBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
 @property (nonatomic, copy) NSArray *venues;
+
 @end
 
 @implementation FQSearchTableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.venues.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FQSearchResultCellIdentifier" forIndexPath:indexPath];
+	
+	FQVenue *venue = self.venues[indexPath.row];
+	
+	[cell.textLabel setText:venue.name];
+	[cell.detailTextLabel setText:venue.categoryName];
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+#pragma mark - Search bar delegate
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+	if (searchBar.text.length >= 3) {
+		[searchBar resignFirstResponder];
+		FQSearchService *searchService = [[FQSearchService alloc] init];
+		[searchService searchVenues:searchBar.text
+				  completionHandler:^(NSArray *venues, NSError *error) {
+					  if (!error) {
+						  self.venues = venues;
+						  dispatch_async(dispatch_get_main_queue(), ^{
+							  [self.tableView reloadData];
+						  });
+					  } else {
+						  UIAlertController * alert =   [UIAlertController
+														alertControllerWithTitle:@"Error"
+														message:error.description
+														preferredStyle:UIAlertControllerStyleAlert];
+						  [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil]];
+						  [self presentViewController:alert animated:YES completion:nil];
+					  }
+		}];
+
+	} else {
+		UIAlertController * alert=   [UIAlertController
+									  alertControllerWithTitle:@"Search to short"
+									  message:@"The search text needs to have at least 3 characteres."
+									  preferredStyle:UIAlertControllerStyleAlert];
+		[alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil]];
+		[self presentViewController:alert animated:YES completion:nil];
+	}
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
